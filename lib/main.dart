170a993aec96b5394ae83c6917128dfcc73a33a2
enlorik <enlorik@users.noreply.github.com>
2026-08-06 14:35:44 +0200

    feat: add Windows and Linux desktop support via sqflite_common_ffi

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
   runApp(
     const ProviderScope(
       child: GrepinkApp(),
